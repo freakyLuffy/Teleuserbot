0094dc843f2d4751800307f76cff2a1cd28a373e
Himanshu <hk062848@gmail.com>
2021-05-31 22:06:45 +0530

    Create forward.py

---
+++ modules/forward.py
@@ -0,0 +1,13 @@
+from start import client
+for_lis=[1359393775]
+@client.on(events.NewMessage(outgoing=True, pattern="^fdl$"))
+async def ddd(target):
+    message = (await target.get_reply_message())
+    await client.delete_messages(target.input_chat, target.id)
+    await client.forward_messages(for_lis[0], message.id, target.chat_id) 
+
+@client.on(events.NewMessage(outgoing=True, pattern="^fdp$"))
+async def ddd(target):
+    message = (await target.get_reply_message())
+    await client.delete_messages(target.input_chat, target.id)
+    await client.forward_messages(for_lis, message.id, target.chat_id) 
