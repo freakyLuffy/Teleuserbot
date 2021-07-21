9319e9d25811cd631852a0256c0bf09a9f0dccd3
freakyLuffy <63852333+freakyLuffy@users.noreply.github.com>
2021-07-21 09:05:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from start import client
 from modules import codeforces,delete,notes,hastebin,pin,pm,user,spam
-white=[1296026566,867524384,1022865230]
+white=[]
 from telethon import TelegramClient,events
 import logging
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
